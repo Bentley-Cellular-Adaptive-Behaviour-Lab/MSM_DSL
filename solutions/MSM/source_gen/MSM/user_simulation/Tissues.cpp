5bc2bda2831a709436a491133a2f44c87e518fb7
meadt <thomas.mead@crick.ac.uk>
2021-02-18 16:57:41 +0000

    Added mbeddr and IEST3 plugins

---
+++ solutions/MSM/source_gen/MSM/user_simulation/Tissues.cpp
@@ -0,0 +1,24 @@
+
+#include "Tissue.h"
+void Tissue_Container::tissue_set_up() {
+    // Created using: Tissues //
+
+    // Cell Type Declarations //
+    Cell_Type *Endothelial_Type;
+
+    // Tissue Type Declarations //
+    Tissue_Type_Cylindrical *Vessel_Type;
+
+    // Coordinate Declarations //
+    Coordinates Vessel_1_Pos;
+
+    // Cell Type Creation //
+    Endothelial_Type = define_cell_type("Endothelial", CELL_SHAPE_SQUARE, 20, 20);
+
+    // Tissue Type Creation //
+    Vessel_Type = define_tissue_type("Vessel", Endothelial_Type, CELL_CONFIGURATION_CYLINDRICAL, 1, 10, 6);
+
+
+    Vessel_1_Pos = Coordinates(100, 10, 10);
+    create_tissue("Vessel_1", Vessel_Type, &(Vessel_1_Pos));
+}
