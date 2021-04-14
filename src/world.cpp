3be692ee0cbeab898107724e7c4fa3cba54c8d7e
meadt <thomas.mead@crick.ac.uk>
2021-04-14 18:00:47 +0100

    Cuboidal gradients now calculate weights based on a single axis.

--- src/world.cpp
+++ src/world.cpp
@@ -120,28 +120,77 @@ void Gradient::calc_exp_env_VEGF(Env* ep) {
     float weight = 1.00f;
 	float starting_protein_level = m_starting_amount;
 
-    vector<float> ep_distances = calculate_dist_from_source(ep);
+	if (m_gradient_shape == GRADIENT_SHAPE_SINKANDSOURCE || m_gradient_shape == GRADIENT_SHAPE_POINT) {
+		vector<float> ep_distances = calculate_dist_from_source(ep);
 
-    if (ep->blood == 0.0f) {
-        // Get fraction of total distance along varied axis, and reduce weight by appropriate amount for that axis.
-        if (x_varying) {
-			if (ep_distances[0] != 0 && m_source_to_sink_distances[0] != 0) {
-				weight = weight * (1 - (ep_distances[0] / m_source_to_sink_distances[0]));
+		if (ep->blood == 0.0f) {
+			// Get fraction of total distance along varied axis, and reduce weight by appropriate amount for that axis.
+			if (x_varying) {
+				if (ep_distances[0] != 0 && m_source_to_sink_distances[0] != 0) {
+					weight = weight * (1 - (ep_distances[0] / m_source_to_sink_distances[0]));
+				}
 			}
-        }
-        if (y_varying) {
-			if (ep_distances[1] != 0 && m_source_to_sink_distances[1] != 0) {
-				weight = weight * (1 - (ep_distances[1] / m_source_to_sink_distances[1]));
+			if (y_varying) {
+				if (ep_distances[1] != 0 && m_source_to_sink_distances[1] != 0) {
+					weight = weight * (1 - (ep_distances[1] / m_source_to_sink_distances[1]));
+				}
 			}
-        }
-        if (z_varying) {
-			if (ep_distances[2] != 0 && m_source_to_sink_distances[2] != 0) {
-				weight = weight * (1 - (ep_distances[2] / m_source_to_sink_distances[2]));
+			if (z_varying) {
+				if (ep_distances[2] != 0 && m_source_to_sink_distances[2] != 0) {
+					weight = weight * (1 - (ep_distances[2] / m_source_to_sink_distances[2]));
+				}
 			}
-        }
-        // Increment VEGF by amount determined by cumulative weights of distance travelled along each varied axis.
-        ep->VEGF += exp(weight) * starting_protein_level;
-    }
+			// Increment VEGF by amount determined by cumulative weights of distance travelled along each varied axis.
+			ep->VEGF += exp(weight) * starting_protein_level;
+		}
+	}
+
+	if (m_gradient_shape == GRADIENT_SHAPE_CUBOIDAL) {
+		if (ep->blood == 0.0f) {
+			if (m_gradient_direction == GRADIENT_DIRECTION_DEC_X) {
+				// Check how far along the x axis from the upper x bound the env agent is and set weight according to
+				// that value as a percentage.
+				float x_dist = (m_centre_position->x + m_cuboidal_width / 2) - ep->Ex;
+				weight = 1 - (x_dist / m_cuboidal_width); // When fully traversed, x_dist = cuboidal width, so set weight to zero.
+				ep->VEGF += exp(weight) * starting_protein_level;
+			}
+			if (m_gradient_direction == GRADIENT_DIRECTION_DEC_Y) {
+				// Check how far along the y axis from the upper y bound the env agent is and set weight according to
+				// that value as a percentage.
+				float y_dist = (m_centre_position->y + m_cuboidal_height / 2) - ep->Ey;
+				weight = 1 - (y_dist / m_cuboidal_height); // When fully traversed, y_dist = cuboidal height, so set weight to zero.
+				ep->VEGF += exp(weight) * starting_protein_level;
+			}
+			if (m_gradient_direction == GRADIENT_DIRECTION_DEC_Z) {
+				// Check how far along the z axis from the upper z bound the env agent is and set weight according to
+				// that value as a percentage.
+				float z_dist = (m_centre_position->z + m_cuboidal_depth / 2) - ep->Ez;
+				weight = 1 - (z_dist / m_cuboidal_depth); // When fully traversed, z_dist = cuboidal depth, so set weight to zero.
+				ep->VEGF += exp(weight) * starting_protein_level;
+			}
+			if (m_gradient_direction == GRADIENT_DIRECTION_INC_X) {
+				// Check how far along the x axis from the lower x bound the env agent is and set weight according to
+				// that value as a percentage.
+				float x_dist = ep->Ex - (m_centre_position->x - m_cuboidal_width / 2);
+				weight = 1 - (x_dist / m_cuboidal_width); // When fully traversed, x_dist = cuboidal width, so set weight to zero.
+				ep->VEGF += exp(weight) * starting_protein_level;
+			}
+			if (m_gradient_direction == GRADIENT_DIRECTION_INC_Y) {
+				// Check how far along the y axis from the lower y bound the env agent is and set weight according to
+				// that value as a percentage.
+				float y_dist = ep->Ey - (m_centre_position->y - m_cuboidal_height / 2);
+				weight = 1 - (y_dist / m_cuboidal_height); // When fully traversed, y_dist = cuboidal height, so set weight to zero.
+				ep->VEGF += exp(weight) * starting_protein_level;
+			}
+			if (m_gradient_direction == GRADIENT_DIRECTION_INC_Z) {
+				// Check how far along the y axis from the lower z bound the env agent is and set weight according to
+				// that value as a percentage.
+				float z_dist = ep->Ez - (m_centre_position->z - m_cuboidal_depth / 2);
+				weight = 1 - (z_dist / m_cuboidal_height); // When fully traversed, y_dist = cuboidal height, so set weight to zero.
+				ep->VEGF += exp(weight) * starting_protein_level;
+			}
+		}
+	}
 }
 
 /*****************************************************************************************
