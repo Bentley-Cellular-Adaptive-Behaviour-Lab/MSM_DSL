f346b5dbd2ec9b08ff9d17b4f868b1267e45182e
Tom <thomas.mead@crick.ac.uk>
2021-05-21 15:26:48 +0100

    Added new protein class to be used with ODE system implementation.

---
+++ src/protein.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tom on 21/05/2021.
+//
+
+#include "protein.h"
---
+++ src/protein.h
@@ -0,0 +1,14 @@
+//
+// Created by Tom on 21/05/2021.
+//
+
+#ifndef SPRINGAGENT_PROTEIN_H
+#define SPRINGAGENT_PROTEIN_H
+
+
+class protein {
+
+};
+
+
+#endif //SPRINGAGENT_PROTEIN_H
