29e93425890f30a84211f85750d2316fde67923b
meadt <thomas.mead@crick.ac.uk>
2021-06-28 11:38:07 +0100

    Split build into three sets depending on test configuration.

---
+++ tests/automated/main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Thomas Mead on 28/06/2021.
+//
+
+#include <iostream>
+#include <algorithm>
+
+void readArgs(int argc, char * argv[]) {
+
+}
+
+void checkArgValues(int argc, char * argv[]) {
+
+}
+
+void print_exit(std::string message) {
+	std::cout << message;
+	exit(1);
+}
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
+
+
