c1ac6c457eeaa47aa8f0ba356b7643faf4259b06
meadt <thomas.mead@crick.ac.uk>
2021-07-01 14:59:43 +0100

    Implemented new tests for memAgent ODEs.

--- tests/automated/helper_ODE.cpp
+++ tests/automated/helper_ODE.cpp
@@ -11,24 +11,124 @@
 
 // Test suite includes
 #include "helper_ODE.h"
+#include "EC.h"
+#include "memAgents.h"
+#include "environment.h"
+#include "protein.h"
 
-void ODEFixtureTest::SetUp() {
+namespace odeint = boost::numeric::odeint;
+
+/*****************************************************************************************
+*  Name:		ODEMemAgentTest::SetUp()
+*  Description: Creates a world with three memAgents, which have a certain level of protein A.
+*	  			The world has a certain level of protein B.
+*	  			The world runs 10 rounds of updating an ODE between A and B, and checks the
+*	  			levels in the memAgent after these.
+*
+*	  			ODE: 1 B -> 1 A every timestep.
+*
+*  Returns:		void
+******************************************************************************************/
+void ODEMemAgentTest::SetUp() {
 	// Setup world container for this test fixture.
 	auto *w_container = new World_Container();
 	addWorldContainer(w_container);
 
-	// Setup world for this test fixture.
+	// Setup world for this test fixture - generates 50x50x50 world w/ no objects.
 	worldContainer->world_setup();
 	auto world = worldContainer->get_world();
 	addWorld(world);
+
+	// Create an uninitialised "dummy" cell for use with the MemAgents.
+	auto dummyCell = new EC(world);
+	world->ECagents.push_back(dummyCell);
+
+	// Create new memAgents.
+	createMemAgents(dummyCell, world);
+
+	// Set protein levels for each env agent.
+	setupEnvironment();
+
+	// Add proteins to memAgents.
+	memAgent1->owned_proteins.push_back(new protein("A", 10, false));
+	memAgent2->owned_proteins.push_back(new protein("A", 10, false));
+	memAgent3->owned_proteins.push_back(new protein("A", 10, false));
+
+	for (int i = 0; i < 10; i++) {
+		runODE(memAgent1);
+		runODE(memAgent2);
+		runODE(memAgent3);
+	}
 }
 
-void ODEFixtureTest::addWorld(World *world) {
+void ODEMemAgentTest::TearDown() {
+
+}
+
+void ODEMemAgentTest::addWorld(World *world) {
 	this->world = world;
 }
 
-void ODEFixtureTest::addWorldContainer(World_Container *worldContainer) {
+void ODEMemAgentTest::addWorldContainer(World_Container *worldContainer) {
 	this->worldContainer = worldContainer;
+}
+
+void ODEMemAgentTest::createMemAgents(EC *dummyCell, World *world) {
+	// Create three memAgents, set their positions and add them to the world.
+	auto memAgent1 = new MemAgent(dummyCell, world); // Isolated.
+	this->memAgent1 = memAgent1;
+	memAgent1->Mx = 25;
+	memAgent1->My = 25;
+	memAgent1->Mz = 25;
+	world->grid[25][25][25].type = const_M;
+
+	auto memAgent2 = new MemAgent(dummyCell, world); // 2 + 3 are adjacent.
+	this->memAgent2 = memAgent2;
+	memAgent2->Mx = 30;
+	memAgent2->My = 30;
+	memAgent2->Mz = 30;
+	world->grid[30][30][30].type = const_M;
+
+	auto memAgent3 = new MemAgent(dummyCell, world); // 2 + 3 are adjacent.
+	this->memAgent3 = memAgent3;
+	memAgent3->Mx = 31;
+	memAgent3->My = 30;
+	memAgent3->Mz = 30;
+	world->grid[31][30][30].type = const_M;
+}
+
+void ODEMemAgentTest::setupEnvironment() {
+	Env *ep;
+	for (int x = 0; x < world->gridXDimensions; x++) {
+		for (int y = 0; y < world->gridYDimensions; y++) {
+			for (int z = 0; z < world->gridYDimensions; z++) {
+				if (world->grid[x][y][z].type == const_E) {
+					auto proteinB = new protein("B", 1, false);
+					ep = world->grid[x][y][z].Eid;
+					ep->owned_proteins.push_back(proteinB);
+				}
+			}
+		}
+	}
+}
+
+void ODEMemAgentTest::memAgentODE_system(const test_ode_states &x, test_ode_states &dxdt, double t) {
+	double A = x[0];
+	double B = x[1];
+
+	dxdt[0] = +x[1]; // Rate of change for species A
+}
+
+void ODEMemAgentTest::runODE(MemAgent *memAgent) {
+	test_ode_states ode_states;
+	odeint::runge_kutta4<test_ode_states> stepper;
+
+	ode_states[0] = memAgent->get_memAgent_protein_level("A"); // Protein A
+	ode_states[1] = memAgent->get_environment_protein_level("B"); // Protein B
+
+	stepper.do_step(memAgentODE_system, ode_states, 0.0, 1);
+
+	memAgent->set_protein_level("A", ode_states[0]);
 }
 
 void constantODE_system(const test_ode_states &x, test_ode_states &dxdt, double t) {
@@ -37,4 +137,12 @@ void constantODE_system(const test_ode_states &x, test_ode_states &dxdt, double 
 
 	dxdt[0] = -5; // Rate of change for species A
 	dxdt[1] = +5; // Rate of change for species B
+}
+
+void linearODE_system(const test_ode_states &x, test_ode_states &dxdt, double t) {
+	double A = x[0];
+	double B = x[1];
+
+	dxdt[0] = -(x[0] * 0.1); // Rate of change for species A
+	dxdt[1] = +(x[0] * 0.1); // Rate of change for species B
 }
--- tests/automated/helper_ODE.h
+++ tests/automated/helper_ODE.h
@@ -11,18 +11,30 @@ typedef boost::array<double, 2> test_ode_states;
 
 class World;
 class World_Container;
+class MemAgent;
 
-class ODEFixtureTest : public ::testing::Test {
+class ODEMemAgentTest : public ::testing::Test {
 protected:
 	void SetUp() override;
+	void TearDown() override;
 public:
 	World *world;
 	World_Container *worldContainer;
 
+	MemAgent *memAgent1;
+	MemAgent *memAgent2;
+	MemAgent *memAgent3;
+
 	void addWorld(World *world);
 	void addWorldContainer(World_Container *worldContainer);
+	void createMemAgents(EC *dummyCell, World *world);
+	void setupEnvironment();
+	void runODE(MemAgent *memAgent);
+
+	static void memAgentODE_system(const test_ode_states &x, test_ode_states &dxdt, double t);
 };
 
 void constantODE_system(const test_ode_states &x, test_ode_states &dxdt, double t);
+void linearODE_system(const test_ode_states &x, test_ode_states &dxdt, double t);
 
 #endif //TESTS_AUTOMATED_AUTOSPRINGAGENT_HELPER_ODE_H
