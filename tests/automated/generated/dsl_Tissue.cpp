ea79452b58d8839dc4714a8ca441fa48cb755bb4
meadt <thomas.mead@crick.ac.uk>
2021-07-19 12:23:30 +0100

    WORKING TESTS

--- tests/automated/generated/dsl_Tissue.cpp
+++ tests/automated/generated/dsl_Tissue.cpp
@@ -2,3 +2,21 @@
 // Created by Thomas Mead on 19/07/2021.
 //
 
+#include <Tissue.h>
+
+void Tissue_Container::tissue_set_up() {
+	// Created using: NewTissues //
+
+	// Cell Type Declarations //
+	Cell_Type *EndoType_Type;
+
+	// Tissue Type Declarations //
+	Tissue_Type_Flat *FlatType_Type;
+
+	// Coordinate Declarations //
+	Coordinates Flat_Pos;
+
+	// Cell Type Creation //
+	EndoType_Type = define_cell_type("EndoType", CELL_SHAPE_SQUARE, 5, 5);
+
+}
