b988e603f565fd99756ac72e4d5855de706fe935
meadt <thomas.mead@crick.ac.uk>
2021-07-19 12:23:30 +0100

    WORKING TESTS

---
+++ cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/meadt/MPSProjects/MSM_DSL/MSM_DSL/cmake-build-debug/_deps/googletest-src
+# Build directory: /Users/meadt/MPSProjects/MSM_DSL/MSM_DSL/cmake-build-debug/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
