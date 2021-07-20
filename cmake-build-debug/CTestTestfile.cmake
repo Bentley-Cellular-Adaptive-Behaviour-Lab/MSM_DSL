45665d5c1ff51d90d41d85a4bbbc6c6918cd9f54
Tom <thomas.mead@crick.ac.uk>
2021-07-20 16:58:41 +0100

    Pre-merge commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Tom/MPSProjects/MSM_DSL
+# Build directory: C:/Users/Tom/MPSProjects/MSM_DSL/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/Tom/MPSProjects/MSM_DSL/cmake-build-debug/autoSpringAgent[1]_include.cmake")
+subdirs("_deps/googletest-build")
