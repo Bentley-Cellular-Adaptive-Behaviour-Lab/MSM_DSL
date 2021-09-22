a302dcb4cb0c7d6dfdfa350afb1adbef722cbbf6
meadt <thomas.mead@crick.ac.uk>
2021-09-22 17:04:35 +0100

    Implemented functions for shape container to handle shape set up.

--- src/dsl/shape/protrusion.cpp
+++ src/dsl/shape/protrusion.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "protrusion.h"
+
+
+// TODO:: WRITE REPLACEMENT FUNCTIONS FOR THESE PROTRUSION BEHAVIOUR FUNCTIONS
+
+// Env * MemAgent::findHighestConc(void); ///guides filopodia direction in VEGF gradient direction
+
+
+// TODO: WRITE REPLACEMENT FUNCTIONS FOR THESE ACTIN TOKEN FUNCTIONS.
+
+// void MemAgent::VEGFRresponse()
+
+// void MemAgent::TokenTrading() OR void MemAgent::ActinFlow()
+
+// void EC::calcCurrentActinUsed()
+
+// bool MemAgent::filExtend()
+
+// bool MemAgent::filRetract()
+
+// void MemAgent::veilAdvance()
+
+// float MemAgent::FilLength(int type)
+
+// void MemAgent::tryActinPassRadiusN(int x, int y, int z, int N)
+
+// void MemAgent::calcRetractDist()
