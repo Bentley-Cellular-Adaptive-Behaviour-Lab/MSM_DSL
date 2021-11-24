3195cc0a623c1cffddddae4d1ac3b01d4b3add55
meadt <thomas.mead@crick.ac.uk>
2021-11-24 11:10:13 +0000

    Removed substrate creation from dsl_world_gen

--- src/generated/dsl_world_gen.cpp
+++ src/generated/dsl_world_gen.cpp
@@ -10,16 +10,9 @@ void World_Container::world_setup() {
 
     World *world;
 
-    Shape_Cuboid *Substrate_shape;
-    Coordinates Substrate_centre;
-
     // WORLD CREATION
 
     world = create_world(60, 32, 32, 1.0);
     m_world = world;
 
-    // GRADIENT CREATION
-
-    // SUBSTRATE CREATION
-    create_substrate(Substrate_shape, &(Substrate_centre), NO_DIRECTION, 1.0);
 }
