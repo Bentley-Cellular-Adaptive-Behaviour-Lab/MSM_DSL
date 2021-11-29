a19076542e14fda49b0762ab3c46896dde1d1b01
Tom <thomas.mead@crick.ac.uk>
2021-11-29 16:29:08 +0000

    Removed less files

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/meadt/MPSProjects/MSM_DSL/MSM_DSL
-# Build directory: /Users/meadt/MPSProjects/MSM_DSL/MSM_DSL/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/meadt/MPSProjects/MSM_DSL/MSM_DSL/cmake-build-debug/autoSpringAgent[1]_include.cmake")
-subdirs("_deps/googletest-build")
