53e2aa0964b1655357ae098bfd5066521efca999
Tom <thomas.mead@crick.ac.uk>
2021-12-09 13:35:55 +0000

    Added shape examples to test folder

---
+++ tests/automated/generated/dsl_shape_gen.h
@@ -0,0 +1,12 @@
+//
+// Created by Thomas Mead on 19/07/2021.
+//
+
+#ifndef SRC_SHAPE_GEN_H
+#define SRC_SHAPE_GEN_H
+
+class Protrusion;
+
+static bool check_FILOPODIA_condition(MemAgent *memAgent);
+
+#endif //SRC_SHAPE_GEN_H
