2ff99f21337f8f516c3efaf027c062513b491391
meadt <thomas.mead@crick.ac.uk>
2022-05-04 14:40:25 +0100

    Commit before testing

---
+++ src/slurmMSM.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=MSM_Simulation
+#SBATCH --ntasks=1
+#SBATCH --time=12:00:00
+#SBATCH --mem=8gb
+#SBATCH --array=1-441
+
+found=false
+
+count=0
+for replicate_number in {1..1}
+do
+for INCREMENT_0 in {0..19}
+do
+for INCREMENT_1 in {0..19}
+do
+count=$((count+1))
+if [ $count == $SLURM_ARRAY_TASK_ID ]
+then
+found=true
+break
+fi
+done
+if [ "$found" = true ]
+then
+break
+fi
+done
+if [ "$found" = true ]
+then
+break
+fi
+done
+INCREMENT_0_VALUE=$(echo '('0.5 '-' 0')/'20'*'$INCREMENT_0'+'0 | bc -l)
+INCREMENT_1_VALUE=$(echo '('0.5 '-' 0')/'20'*'$INCREMENT_1'+'0 | bc -l)
+./springAgent  "$INCREMENT_0_VALUE" "$INCREMENT_1_VALUE" $replicate_number 1
