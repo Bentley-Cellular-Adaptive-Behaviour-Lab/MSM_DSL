2617adec4425e9dd68b0a02e414b358853de5999
meadt <thomas.mead@crick.ac.uk>
2022-12-06 10:37:04 +0000

    Changed tissues to m_tissues in tissue.cpp

--- src/dsl/tissue/tissue.cpp
+++ src/dsl/tissue/tissue.cpp
@@ -502,8 +502,8 @@ bool Tissue_Vessel::check_boundaries() {
         }
     }
 
-    for (int i = 0; i < m_tissue_container->tissues.size(); i++) {
-        current_tissue = m_tissue_container->tissues[i];
+    for (int i = 0; i < m_tissue_container->m_tissues.size(); i++) {
+        current_tissue = m_tissue_container->m_tissues[i];
         if (current_tissue != this) {
             if (current_tissue->m_tissue_type->m_tissue_configuration == CELL_CONFIGURATION_FLAT) {
                 auto *current_monolayer = dynamic_cast<Tissue_Monolayer *>(current_tissue);
@@ -917,8 +917,8 @@ bool Tissue_Monolayer::check_boundaries() {
         }
     }
 
-    for (int i = 0; i < m_tissue_container->tissues.size(); i++) {
-        current_tissue = m_tissue_container->tissues[i];
+    for (int i = 0; i < m_tissue_container->m_tissues.size(); i++) {
+        current_tissue = m_tissue_container->m_tissues[i];
         if (current_tissue != this) {
             if (current_tissue->m_tissue_type->m_tissue_configuration == CELL_CONFIGURATION_FLAT) {
                 auto *current_monolayer = dynamic_cast<Tissue_Monolayer *>(current_tissue);
