df8e31a362e12808db2bfad8b13123afdd90be2d
meadt <thomas.mead@crick.ac.uk>
2022-12-09 11:20:09 +0000

    Emptied ODE functions in test suite

--- tests/automated/generated/dsl_species_gen.cpp
+++ tests/automated/generated/dsl_species_gen.cpp
@@ -42,76 +42,76 @@ void ODEs::check_cell_only_ODEs(EC *ec) {
 }
 
 void ODEs::Endothelial_memAgent_system(const Endothelial_memAgent_ode_states &x, Endothelial_memAgent_ode_states &dxdt, double t) {
-    double VEGF = x[0];
-    double VEGFR = x[1];
-    double VEGF_VEGFR = x[2];
-    double NOTCH = x[3];
-    double DLL4 = x[4];
-    double NOTCH_DLL4 = x[5];
-    double VEGF_VEGFR_FORWARD = calc_VEGF_VEGFR_FORWARD_rate();
-    double VEGF_VEGFR_REVERSE = calc_VEGF_VEGFR_REVERSE_rate();
-    double NOTCH_DLL4_FORWARD = calc_NOTCH_DLL4_FORWARD_rate();
-    double NOTCH_DLL4_REVERSE = calc_NOTCH_DLL4_REVERSE_rate();
-    dxdt[0] = -VEGF_VEGFR_FORWARD*1+VEGF_VEGFR_REVERSE*1;
-    dxdt[1] = -VEGF_VEGFR_FORWARD*1+VEGF_VEGFR_REVERSE*1;
-    dxdt[2] = +VEGF_VEGFR_FORWARD*1-VEGF_VEGFR_REVERSE*1;
-    dxdt[3] = -NOTCH_DLL4_FORWARD*1+NOTCH_DLL4_REVERSE*1;
-    dxdt[4] = -NOTCH_DLL4_FORWARD*1+NOTCH_DLL4_REVERSE*1;
-    dxdt[5] = +NOTCH_DLL4_FORWARD*1-NOTCH_DLL4_REVERSE*1;
+//    double VEGF = x[0];
+//    double VEGFR = x[1];
+//    double VEGF_VEGFR = x[2];
+//    double NOTCH = x[3];
+//    double DLL4 = x[4];
+//    double NOTCH_DLL4 = x[5];
+//    double VEGF_VEGFR_FORWARD = calc_VEGF_VEGFR_FORWARD_rate();
+//    double VEGF_VEGFR_REVERSE = calc_VEGF_VEGFR_REVERSE_rate();
+//    double NOTCH_DLL4_FORWARD = calc_NOTCH_DLL4_FORWARD_rate();
+//    double NOTCH_DLL4_REVERSE = calc_NOTCH_DLL4_REVERSE_rate();
+//    dxdt[0] = -VEGF_VEGFR_FORWARD*1+VEGF_VEGFR_REVERSE*1;
+//    dxdt[1] = -VEGF_VEGFR_FORWARD*1+VEGF_VEGFR_REVERSE*1;
+//    dxdt[2] = +VEGF_VEGFR_FORWARD*1-VEGF_VEGFR_REVERSE*1;
+//    dxdt[3] = -NOTCH_DLL4_FORWARD*1+NOTCH_DLL4_REVERSE*1;
+//    dxdt[4] = -NOTCH_DLL4_FORWARD*1+NOTCH_DLL4_REVERSE*1;
+//    dxdt[5] = +NOTCH_DLL4_FORWARD*1-NOTCH_DLL4_REVERSE*1;
 }
 
 void ODEs::Endothelial_run_memAgent_ODEs(MemAgent *memAgent) {
-    Endothelial_memAgent_ode_states current_states;
-    Endothelial_memAgent_ode_states new_states;
-    odeint::euler<Endothelial_memAgent_ode_states> stepper;
-
-    current_states[0] = memAgent->get_environment_level("VEGF", true);
-    current_states[1] = memAgent->get_local_protein_level("VEGFR");
-    current_states[2] = memAgent->get_local_protein_level("VEGF_VEGFR");
-    current_states[3] = memAgent->get_junction_protein_level("NOTCH", true);
-    current_states[4] = memAgent->get_junction_protein_level("DLL4", true);
-    current_states[5] = memAgent->get_junction_protein_level("NOTCH_DLL4", true);
-
-    stepper.do_step(Endothelial_memAgent_system, current_states, 0.0, new_states, 1);
-
-    memAgent->distribute_calculated_proteins("VEGFR", new_states[1], true, false, PROTEIN_LOCATION_MEMBRANE);
-    memAgent->distribute_calculated_proteins("VEGF_VEGFR", new_states[2], true, false, PROTEIN_LOCATION_MEMBRANE);
-    memAgent->distribute_calculated_proteins("NOTCH", new_states[3], true, false, PROTEIN_LOCATION_JUNCTION);
-    memAgent->distribute_calculated_proteins("DLL4", new_states[4], false, true, PROTEIN_LOCATION_JUNCTION);
-    memAgent->distribute_calculated_proteins("NOTCH_DLL4", new_states[5], false, true, PROTEIN_LOCATION_JUNCTION);
+//    Endothelial_memAgent_ode_states current_states;
+//    Endothelial_memAgent_ode_states new_states;
+//    odeint::euler<Endothelial_memAgent_ode_states> stepper;
+//
+//    current_states[0] = memAgent->get_environment_level("VEGF", true);
+//    current_states[1] = memAgent->get_local_protein_level("VEGFR");
+//    current_states[2] = memAgent->get_local_protein_level("VEGF_VEGFR");
+//    current_states[3] = memAgent->get_junction_protein_level("NOTCH", true);
+//    current_states[4] = memAgent->get_junction_protein_level("DLL4", true);
+//    current_states[5] = memAgent->get_junction_protein_level("NOTCH_DLL4", true);
+//
+//    stepper.do_step(Endothelial_memAgent_system, current_states, 0.0, new_states, 1);
+//
+//    memAgent->distribute_calculated_proteins("VEGFR", new_states[1], true, false, PROTEIN_LOCATION_MEMBRANE);
+//    memAgent->distribute_calculated_proteins("VEGF_VEGFR", new_states[2], true, false, PROTEIN_LOCATION_MEMBRANE);
+//    memAgent->distribute_calculated_proteins("NOTCH", new_states[3], true, false, PROTEIN_LOCATION_JUNCTION);
+//    memAgent->distribute_calculated_proteins("DLL4", new_states[4], false, true, PROTEIN_LOCATION_JUNCTION);
+//    memAgent->distribute_calculated_proteins("NOTCH_DLL4", new_states[5], false, true, PROTEIN_LOCATION_JUNCTION);
 }
 
 void ODEs::Endothelial_cell_system(const Endothelial_cell_ode_states &x, Endothelial_cell_ode_states &dxdt, double t) {
-    double NOTCH_DLL4 = x[0];
-    double VEGFR = x[1];
-    double VEGF_VEGFR = x[2];
-    double DLL4 = x[3];
-    double VEGFR_INHIBITION_MOD = calc_VEGFR_INHIBITION_MOD_rate();
-    double NOTCH_UPREGULATION_MOD = calc_NOTCH_UPREGULATION_MOD_rate(VEGFR, VEGF_VEGFR);
-    dxdt[0] = 0;
-    dxdt[1] = -VEGFR_INHIBITION_MOD;
-    dxdt[2] = 0;
-    dxdt[3] = +NOTCH_UPREGULATION_MOD;
+//    double NOTCH_DLL4 = x[0];
+//    double VEGFR = x[1];
+//    double VEGF_VEGFR = x[2];
+//    double DLL4 = x[3];
+//    double VEGFR_INHIBITION_MOD = calc_VEGFR_INHIBITION_MOD_rate();
+//    double NOTCH_UPREGULATION_MOD = calc_NOTCH_UPREGULATION_MOD_rate(VEGFR, VEGF_VEGFR);
+//    dxdt[0] = 0;
+//    dxdt[1] = -VEGFR_INHIBITION_MOD;
+//    dxdt[2] = 0;
+//    dxdt[3] = +NOTCH_UPREGULATION_MOD;
 }
 
 void ODEs::Endothelial_run_cell_ODEs(EC *ec) {
-    Endothelial_cell_ode_states current_states;
-    Endothelial_cell_ode_states new_states;
-    odeint::euler<Endothelial_cell_ode_states> stepper;
-
-    current_states[0] = ec->get_cell_protein_level("NOTCH_DLL4",0);
-    current_states[1] = ec->get_cell_protein_level("VEGFR",0);
-    current_states[2] = ec->get_cell_protein_level("VEGF_VEGFR",0);
-    current_states[3] = ec->get_cell_protein_level("DLL4",0);
-
-    stepper.do_step(Endothelial_cell_system, current_states, 0.0, new_states, 1);
-
-    ec->set_cell_protein_level("VEGFR", new_states[1],1);
-    ec->set_cell_protein_level("DLL4", new_states[3],1);
+//    Endothelial_cell_ode_states current_states;
+//    Endothelial_cell_ode_states new_states;
+//    odeint::euler<Endothelial_cell_ode_states> stepper;
+//
+//    current_states[0] = ec->get_cell_protein_level("NOTCH_DLL4",0);
+//    current_states[1] = ec->get_cell_protein_level("VEGFR",0);
+//    current_states[2] = ec->get_cell_protein_level("VEGF_VEGFR",0);
+//    current_states[3] = ec->get_cell_protein_level("DLL4",0);
+//
+//    stepper.do_step(Endothelial_cell_system, current_states, 0.0, new_states, 1);
+//
+//    ec->set_cell_protein_level("VEGFR", new_states[1],1);
+//    ec->set_cell_protein_level("DLL4", new_states[3],1);
 }
 
 void ODEs::Endothelial_run_cell_only_ODEs(EC *ec) {
-    Endothelial_cell_ode_states states;
+//    Endothelial_cell_ode_states states;
 
 }
 
