6069c8466dc9a91cf03eabe60b59c023c697c5a8
meadt <thomas.mead@crick.ac.uk>
2023-01-09 14:27:12 +0000

    calc_level is now a const double

--- src/dsl/world/gradient.h
+++ src/dsl/world/gradient.h
@@ -98,7 +98,7 @@ public:
 
     std::vector<float> calculate_dist_from_source(Env* ep) const;
 
-    void add_env_protein(Env *ep, float calc_level);
+    void add_env_protein(Env *ep, const double calc_level) const;
 
     //Generic functions for creating new protein gradients.
     void calc_constant_env_protein(Env* ep);
